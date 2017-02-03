023d349cd2107aac5ad481c3b42a33e5343b20cb
mmminari <mmminari@smtown.com>
2017-02-03 16:00:39 +0900

    first commit add .gitignore file

---
+++ GoogleLogin/GoogleLogin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoogleLogin
+//
+//  Created by 김민아 on 2017. 2. 3..
+//  Copyright © 2017년 김민아. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GoogleLogin/GoogleLogin/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GoogleLogin
+//
+//  Created by 김민아 on 2017. 2. 3..
+//  Copyright © 2017년 김민아. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
